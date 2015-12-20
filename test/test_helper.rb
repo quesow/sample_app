c5f426ed2ee15fbd2b84ec91a7b72d88357c4f04
JESUA BETANCOR ALEMAN <JBETANCOR.0011@GMAIL>
2015-12-20 23:03:02 +0000

    Guard configured

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,20 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
+  # order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
+=begin
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -8,3 +22,4 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+=end
